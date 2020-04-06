b6b972b66bb93a757c13fc7bd7ebe29557f2c185
Nikhil Gautam <nikhilgautam303@gmail.com>
2020-04-07 01:35:19 +0530

    changed api url

--- src/app/components/image/image.component.ts
+++ src/app/components/image/image.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AnyMxRecord } from 'dns';
 import { Location } from '@angular/common';
 
 @Component({
